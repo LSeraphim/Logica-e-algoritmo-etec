d24e501fdda802b2f21a0911ca404b6929511cbc
Leo.Seraphim <82623796+LSeraphim@users.noreply.github.com>
2023-11-16 21:45:15 -0300

    Update atvd4.js

--- Lógica e Algoritimos JS/vetor/atvd4.js
+++ Lógica e Algoritimos JS/vetor/atvd4.js
@@ -1,8 +1,20 @@
+// var x = [];
+
+// for (let i = 0; i < 5; i++) {
+//     var n = parseInt(prompt('Digite um número: '));
+//     x.push(n);
+// }
+// x = x.sort();
+// console.log(x[4]);
+
 var x = [];
+var maior = null;
 
 for (let i = 0; i < 5; i++) {
     var n = parseInt(prompt('Digite um número: '));
-    x.push(n);
+    x.unshift(n);
+    if(x[0] > maior) {
+        maior = x[0];
+    }
 }
-x = x.sort();
-console.log(x[4]);
+console.log(maior);
